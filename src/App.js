9d892a68849cf5fdc5057e5784a7443378c2a8fd
RajrupDasid <rajrupdasid@outlook.com>
2021-09-29 12:25:23 +0530

    React ck editor front end error

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component } from 'react';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import axios from 'axios';
+class App extends Component{
+  state={
+    contents:[],
+    post:``
+  };
+  onButtonSubmit=(Event)=>{
+    Event.preventDefault();
+    console.log(this.state.post);
+    axios.defaults.xsrfHeaderName="X-CSRFToken";
+    axios.post('http://localhost:8000/post/',
+    {content:this.state.post},
+    {headers:{'Authorization':'Token 1c345d6053df8ecbec3c35deeec44e38c61aac24'}
+  })
+  . then(response=>{
+    console.log(response);
+    this.setState({
+      contents:response.data
+    })
+  });
+  }
+  createMarkup=(markup)=>{
+    return{__html:markup}
+  }
+  render(){
+    const ckeditorList=this.state.contents.map(content=>{
+      return(
+        <div key={content.id}>
+          <div dangerouslySetInnerHTML={this,this.createMarkup(content.content)}></div>
+          <hr/>
+        </div>
+      )
+    })
+    return (
+      <div className="App">
+          {ckeditorList}
+          <h2>Hello from CKeditor</h2>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <CKEditor
+              editor={ ClassicEditor }
+              onChange={ ( event, editor ) => {
+                  this.setState({post: editor.getData()});
+              }}
+          />
+          <button onClick={this.onButtonSubmit}>Submit</button>
+      </div>
   );
+  }
 }
-
 export default App;
